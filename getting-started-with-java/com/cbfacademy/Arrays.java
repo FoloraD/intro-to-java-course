9d74edbc8637e0ca014b6df59b281907da338219
FoloraD <fduang@gmail.com>
2023-06-05 13:59:04 +0100

    WIP Start Array exercises/homework

---
+++ getting-started-with-java/com/cbfacademy/Arrays.java
@@ -0,0 +1,19 @@
+package com.cbfacademy;
+
+public class Arrays {
+    public static void main(String... args) {
+        //TODO create an array of 8 Integer elements,
+        int[] numbers = {94, 23, 47, 52, 6, 17, 34, 78};
+
+        //TODO create an array of 12 Float elements
+
+
+        //TODO create an array of 5 Double elements
+        //TODO create an array of 6 Boolean elements
+        //TODO print their 5th element to screen
+        
+        System.out.println(numbers[5]); //output 17
+
+
+    };
+}
