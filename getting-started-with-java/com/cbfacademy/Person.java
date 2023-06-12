598c88f754950c695aad0306e239f8b83aaef2ee
FoloraD <fduang@gmail.com>
2023-06-12 20:06:27 +0100

    add comments to person class

--- getting-started-with-java/com/cbfacademy/Person.java
+++ getting-started-with-java/com/cbfacademy/Person.java
@@ -2,18 +2,22 @@ package com.cbfacademy;
 
 //Person class represents details about a person
 public class Person {
+
+    //attributes
     final static String SPECIES = "Human";
 
     String firstName;
     String lastName;
     int age;
 
+    //constructor allows you set values of the person object (aka instance of a class)
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
+    //method of the person object
     public String getDetails() {
         return String.format(
             "My name is %s %s, and I am %s years young. I belong to the %s species.", this.firstName, this.lastName, this.age, SPECIES
